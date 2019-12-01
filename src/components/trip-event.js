02d080d7ada20cdf1e717deac71bbcf4b686f07d
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-01 15:13:05 +0200

    Replace rendering data from mock-object

--- src/components/trip-event.js
+++ src/components/trip-event.js
@@ -1,34 +1,69 @@
-export const createTripEventTemplate = () => `<li class="trip-events__item">
+import {formatTime, convertMinutesToHours} from '../utils';
+
+
+const generateAdditionalServices = (options) => {
+  return Array.from(options)
+    .map((option) => {
+      const {type, name, price} = option;
+
+      return (
+        `<li class="event__offer">
+          <span class="event__offer-title">${type} ${name}</span>
+          &plus;
+          &euro;&nbsp;<span class="event__offer-price">${price}</span>
+         </li>`
+      );
+    })
+    .join(`\n`);
+};
+
+
+export const createTripEventTemplate = (event) => {
+
+  const {type, city, price, time, additionalServices} = event;
+
+  const {start, end, duration} = time;
+
+  const startTime = formatTime(start);
+  const endTime = formatTime(end);
+
+  const startDate = formatTime(start, true);
+  const endDate = formatTime(end, true);
+
+  const durationTime = convertMinutesToHours(duration);
+
+  const offers = generateAdditionalServices(additionalServices);
+
+  return (
+    `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi to airport</h3>
+        <h3 class="event__title">${type} to ${city}</h3>
 
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="${startDate}">${startTime}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="${endDate}">${endTime}</time>
           </p>
-          <p class="event__duration">1H 30M</p>
+          <p class="event__duration">${durationTime}</p>
         </div>
 
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
         </p>
 
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">20</span>
-           </li>
+          ${offers}
         </ul>
 
         <button class="event__rollup-btn" type="button">
           <span class="visually-hidden">Open event</span>
         </button>
       </div>
-    </li>`;
+    </li>`
+  );
+};
