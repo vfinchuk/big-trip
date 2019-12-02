b3ac3a1e3f2b5048680ecddcab88ea3430fea402
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-03 00:45:30 +0200

    Create TripInfo class

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,5 +1,30 @@
+import {createElement} from '../utils';
+
 export const createTripInfoTemplate = () => `<div class="trip-info__main">
       <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
 
       <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
     </div>`;
+
+
+export default class TripInfo {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripInfoTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+};
