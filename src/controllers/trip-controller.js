3634226396f7d9e711e072c8a83e8ca89c66d421
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-10 00:25:31 +0200

    Add sorting method on board rendering

--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -1,10 +1,11 @@
+import {moment} from '../utils/common';
 import {render, RenderPosition, replace} from '../utils/render';
 import {groupTripPointsByDay} from '../mock/trip-point';
 
 import TripPointEditComponent from '../components/trip-point-edit';
 import TripPointComponent from '../components/trip-point';
 
-import SortComponent from '../components/sort';
+import SortComponent, {SortType} from '../components/sort';
 import BoardComponent from '../components/board';
 import TripDayComponent from '../components/trip-day';
 import NoPointsComponent from '../components/no-points';
@@ -45,6 +46,42 @@ const renderTripPoint = (dayItemElement, point) => {
   render(dayItemElement, tripPoint, RenderPosition.BEFOREEND);
 };
 
+/**
+ *
+ * @param {Object} board
+ * @param {Object} dayElement
+ * @param {Array} points
+ */
+function renderTripPoints(board, dayElement, points) {
+  render(board.getElement(), dayElement, RenderPosition.BEFOREEND);
+
+  const tripDayItemPointList = board.getElement().querySelector(`.trip-events__list`);
+
+  points.map((point) => {
+    renderTripPoint(tripDayItemPointList, point);
+  });
+}
+
+/**
+ *
+ * @param {Object} board
+ * @param {HTMLElement} dayElement
+ * @param {Array} pointsByDay
+ */
+function renderTripPointsByDays(board, dayElement, pointsByDay) {
+  [...pointsByDay].map((day, index) => {
+
+    render(board.getElement(), dayElement(day[1].date, day[1].counter), RenderPosition.BEFOREEND);
+
+    const tripDayItemPointList = board.getElement()
+      .querySelectorAll(`.trip-days__item`)[index]
+      .querySelector(`.trip-events__list`);
+
+    day[1].points.map((point) => {
+      renderTripPoint(tripDayItemPointList, point);
+    });
+  });
+}
 
 export default class TripController {
   constructor(container) {
@@ -55,39 +92,76 @@ export default class TripController {
     this._noPointsComponent = new NoPointsComponent();
   }
 
+  /**
+   * Return TripDayComponent
+   * @param {Object|null} date
+   * @param {int|null} counter
+   * @return {TripDay}
+   * @private
+   */
   _createTripDay(date, counter) {
     return new TripDayComponent(date, counter);
   }
 
+  /**
+   * Render trip board
+   * @param {Array} points
+   */
   render(points) {
 
     if (points.length < 0) {
-      /* Rendering no trip points massage */
+      /**
+       * Rendering no trip points massage
+       */
       render(this._container, this._noPointsComponent, RenderPosition.BEFOREEND);
     } else {
 
-      /* Rendering sort and tripBoard elements */
+      /**
+       * Rendering sort and tripBoard elements
+       */
       render(this._container, this._sortComponent, RenderPosition.BEFOREEND);
       render(this._container, this._boardComponent, RenderPosition.BEFOREEND);
 
-      /* Rendering trip day elements */
-      const tripPointsByDay = groupTripPointsByDay(points);
+      /**
+       * Render trip points
+       */
+      const pointsByDay = groupTripPointsByDay(points);
+      renderTripPointsByDays(this._boardComponent, this._createTripDay, pointsByDay);
 
-      [...tripPointsByDay].map((day, index) => {
+      /**
+       * Sorting event listener
+       */
+      this._sortComponent.setSortTypeChangeHandler((sortType) => {
+        let sortedPoints = [];
+        switch (sortType) {
+          case SortType.TIME:
+            sortedPoints = points.slice().sort((a, b) => {
+              const durationA = moment.duration(
+                  moment(a.dateEnd).diff(moment(a.dateStart))
+              );
+              const durationB = moment.duration(
+                  moment(b.dateEnd).diff(moment(b.dateStart))
+              );
 
-        render(this._boardComponent.getElement(), this._createTripDay(day[1].date, day[1].counter), RenderPosition.BEFOREEND);
+              return durationB - durationA;
+            });
+            break;
+          case SortType.PRICE:
+            sortedPoints = points.slice().sort((a, b) => b.price - a.price);
+            break;
+          case SortType.DEFAULT:
+            sortedPoints = groupTripPointsByDay(points);
+            break;
+        }
 
-        const tripDayItemPointList = this._boardComponent.getElement()
-          .querySelectorAll(`.trip-days__item`)[index]
-          .querySelector(`.trip-events__list`);
+        this._boardComponent.getElement().innerHTML = ``;
 
-        day[1].points.map((point) => {
-          renderTripPoint(tripDayItemPointList, point);
-        });
+        if (sortType === SortType.DEFAULT) {
+          renderTripPointsByDays(this._boardComponent, this._createTripDay, sortedPoints);
+        } else {
+          renderTripPoints(this._boardComponent, this._createTripDay(null, null), sortedPoints);
+        }
       });
-
     }
-
   }
-
 }
