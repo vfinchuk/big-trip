6bfebd41ad8b52ee31825e744acb1a01a93de64c
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-25 13:25:40 +0200

    Add event handlers

--- src/components/trip-point-edit.js
+++ src/components/trip-point-edit.js
@@ -1,4 +1,3 @@
-import flatpickr from 'flatpickr';
 import AbstractSmartComponent from './abstract-smart-component';
 import {EventTypeEnum, LOCATIONS} from '../mock/consts';
 import moment from 'moment';
@@ -104,83 +103,85 @@ const createTripPointEditTemplate = (point) => {
   const images = createPhotosMarkup(photos);
 
   return (
-    `<form class="trip-events__item  event  event--edit" action="#" method="post">
-      <header class="event__header">
-        <div class="event__type-wrapper">
-          <label class="event__type  event__type-btn" for="event-type-toggle-1">
-            <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/${type.code}.png" alt="Event type icon">
-          </label>
-          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
-          <div class="event__type-list">
-            <fieldset class="event__type-group">
-              <legend class="visually-hidden">Transfer</legend>
-              ${transferTypes}
-            </fieldset>
-
-            <fieldset class="event__type-group">
-              <legend class="visually-hidden">Activity</legend>
-              ${activityTypes}
-            </fieldset>
-          </div>
-        </div>
-
-        <div class="event__field-group  event__field-group--destination">
-          <label class="event__label  event__type-output" for="event-destination-1">
-             ${type.code} ${getEventPlaceholder(type)} 
-          </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${location.name}" list="destination-list-1">
-          <datalist id="destination-list-1">
-            ${destinations}
-          </datalist>
-        </div>
-
-        <div class="event__field-group  event__field-group--time">
-          <label class="visually-hidden" for="event-start-time-1">
-            From
-          </label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startDate} ${startTime}">
-          &mdash;
-          <label class="visually-hidden" for="event-end-time-1">
-            To
-          </label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endDate} ${endTime}">
-        </div>
-
-        <div class="event__field-group  event__field-group--price">
-          <label class="event__label" for="event-price-1">
-            <span class="visually-hidden">Price</span>
-            &euro;
-          </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
-        </div>
-
-        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
-      </header>
-      <section class="event__details">
-
-        <section class="event__section  event__section--offers">
-          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-          <div class="event__available-offers">
-            ${offersList}
-          </div>
-        </section>
-
-        <section class="event__section  event__section--destination">
-          <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-          <p class="event__destination-description">${description}</p>
-
-          <div class="event__photos-container">
-            <div class="event__photos-tape">
-              ${images}
+    `<li class="trip-events__item">
+      <form class="event  event--edit" action="#" method="post">
+        <header class="event__header">
+          <div class="event__type-wrapper">
+            <label class="event__type  event__type-btn" for="event-type-toggle-1">
+              <span class="visually-hidden">Choose event type</span>
+              <img class="event__type-icon" width="17" height="17" src="img/icons/${type.code}.png" alt="Event type icon">
+            </label>
+            <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+  
+            <div class="event__type-list">
+              <fieldset class="event__type-group">
+                <legend class="visually-hidden">Transfer</legend>
+                ${transferTypes}
+              </fieldset>
+  
+              <fieldset class="event__type-group">
+                <legend class="visually-hidden">Activity</legend>
+                ${activityTypes}
+              </fieldset>
             </div>
           </div>
+  
+          <div class="event__field-group  event__field-group--destination">
+            <label class="event__label  event__type-output" for="event-destination-1">
+               ${type.code} ${getEventPlaceholder(type)} 
+            </label>
+            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${location.name}" list="destination-list-1">
+            <datalist id="destination-list-1">
+              ${destinations}
+            </datalist>
+          </div>
+  
+          <div class="event__field-group  event__field-group--time">
+            <label class="visually-hidden" for="event-start-time-1">
+              From
+            </label>
+            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startDate} ${startTime}">
+            &mdash;
+            <label class="visually-hidden" for="event-end-time-1">
+              To
+            </label>
+            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endDate} ${endTime}">
+          </div>
+  
+          <div class="event__field-group  event__field-group--price">
+            <label class="event__label" for="event-price-1">
+              <span class="visually-hidden">Price</span>
+              &euro;
+            </label>
+            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
+          </div>
+  
+          <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+          <button class="event__reset-btn" type="reset">Cancel</button>
+        </header>
+        <section class="event__details">
+  
+          <section class="event__section  event__section--offers">
+            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+  
+            <div class="event__available-offers">
+              ${offersList}
+            </div>
+          </section>
+  
+          <section class="event__section  event__section--destination">
+            <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+            <p class="event__destination-description">${description}</p>
+  
+            <div class="event__photos-container">
+              <div class="event__photos-tape">
+                ${images}
+              </div>
+            </div>
+          </section>
         </section>
-      </section>
-    </form>`
+      </form>
+    </li>`
   );
 };
 
@@ -190,30 +191,95 @@ export default class TripPointEdit extends AbstractSmartComponent {
     super();
 
     this._point = point;
+
+    this._changePointTypeHandler = this._changePointTypeHandler.bind(this);
+    this._changePriceHandler = this._changePriceHandler.bind(this);
+    this._changeDestinationHandler = this._changeDestinationHandler.bind(this);
+
+   this._subscribeEventsHandler();
+  }
+
+  recoveryListeners() {
+    this._subscribeEventsHandler();
   }
 
   getTemplate() {
     return createTripPointEditTemplate(this._point);
   }
 
-  recoveryListeners() {
+  _subscribeEventsHandler() {
+    const element = this.getElement();
 
+    element.querySelectorAll(`.event__type-input`).forEach((input) => {
+      input.addEventListener(`click`, this._changePointTypeHandler);
+    });
+
+    element.querySelector(`.event__input--price`)
+      .addEventListener(`change`, this._changePriceHandler);
+
+    element.querySelector(`.event__input--destination`)
+      .addEventListener(`change`, this._changeDestinationHandler);
   }
+
 
   rerender() {
     super.rerender();
   }
 
-  reset() {
-    const point = this._point;
-  }
 
-  setSubmitHandler(handler) {
-    this.getElement().addEventListener(`submit`, (evt) => {
+  set saveHandler(callback) {
+    this.registerObserver(`form`, `submit`, (evt) => {
       evt.preventDefault();
-      handler();
+      callback(this._point);
     });
   }
 
+  /**
+   *
+   * @param {Function} callback
+   */
+  set closeHandler(callback) {
+    this.registerObserver(`form`, `reset`, (evt) => {
+      evt.preventDefault();
+      callback();
+    });
+  }
+
+  /**
+   * Change trip point type handler
+   * @param {Event} evt
+   * @private
+   */
+  _changePointTypeHandler(evt) {
+    if (evt.target.value === this._point.type.code) {
+      return;
+    }
+
+    this._point.type = Object.assign({}, EventTypeEnum[evt.target.value.toUpperCase().replace(/-.+$/, ``)]);
+    this.rerender();
+  }
+
+  /**
+   * Change price handler
+   * @param {Event} evt
+   * @private
+   */
+  _changePriceHandler(evt) {
+    if (evt.target.value === this._point.price) {
+      return;
+    }
+
+    this._point.price = evt.target.value;
+    this.rerender();
+  }
+
+  /**
+   * Change point destination handler
+   * @param {Event} evt
+   * @private
+   */
+  _changeDestinationHandler(evt) {
+
+  }
 
 }
